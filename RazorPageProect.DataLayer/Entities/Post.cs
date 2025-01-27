fdc19cf284b93ed251091bb0f02a437ed249f792
Mehdi Heidari <hymehdi84@gmail.com>
2025-01-27 20:26:14 +0300

    add entity post

---
+++ RazorPageProect.DataLayer/Entities/Post.cs
@@ -0,0 +1,9 @@
+﻿namespace RazorPageProect.DataLayer.Entities;
+
+public class Post
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int Visit { get; set; }
+    public string slug { get; set; }
+}

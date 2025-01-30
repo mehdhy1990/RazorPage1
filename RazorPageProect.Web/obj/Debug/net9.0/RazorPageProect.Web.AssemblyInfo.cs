e946d075be54816e1fc3df7948c6e33ab542451e
Mehdi Heidari <hymehdi84@gmail.com>
2025-01-31 00:24:51 +0300

    footer partial view added safe and sound

--- RazorPageProect.Web/obj/Debug/net9.0/RazorPageProect.Web.AssemblyInfo.cs
+++ RazorPageProect.Web/obj/Debug/net9.0/RazorPageProect.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPageProect.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b01f685cd747b39ad36b648fbb837a1868f4c92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a08069c052546d71e1b1de3725713156062984f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPageProect.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPageProect.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

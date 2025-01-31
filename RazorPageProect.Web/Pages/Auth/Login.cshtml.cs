a255b08f93a86127840b01d4df342dc716eebcef
Mehdi Heidari <hymehdi84@gmail.com>
2025-01-31 17:30:12 +0300

    Login and register rezor pages created

---
+++ RazorPageProect.Web/Pages/Auth/Login.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPageProect.Web.Pages.Auth;
+
+public class Login : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ RazorPageProect.Web/Pages/Auth/Register.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPageProect.Web.Pages.Auth;
+
+public class Register : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}

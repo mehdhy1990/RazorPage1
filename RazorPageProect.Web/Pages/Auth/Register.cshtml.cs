1e926dec9234ea56c8036cd2b0291445f3a21cd8
Mehdi Heidari <hymehdi84@gmail.com>
2025-02-01 20:58:07 +0300

    added front end validation script

--- RazorPageProect.Web/Pages/Auth/Register.cshtml.cs
+++ RazorPageProect.Web/Pages/Auth/Register.cshtml.cs
@@ -7,32 +7,35 @@ namespace RazorPageProect.Web.Pages.Auth;
 
 public class Register : PageModel
 {
-   private IUserService _userService;
+    private IUserService _userService;
 
-   #region Properties
-   [Display(Name = "Username")]
-[Required(ErrorMessage = "Username is required")]
-   public string UserName { get; set; }
-   [Display(Name = "Email")]
-   [Required(ErrorMessage = "Email is required")]
-   public string FullName { get; set; }
-   [Display(Name = "Password")]
-   [Required(ErrorMessage = "Password is required")]
-   public string Password { get; set; }
+    #region Properties
 
-   #endregion
+    [Display(Name = "Username")]
+    [Required(ErrorMessage = "Username is required")]
+    public string UserName { get; set; }
+
+    [Display(Name = "Email")]
+    [Required(ErrorMessage = "Email is required")]
+    public string FullName { get; set; }
+
+    [Display(Name = "Password")]
+    [Required(ErrorMessage = "Password is required")]
+    public string Password { get; set; }
+
+    #endregion
 
     public Register(IUserService userService)
     {
         _userService = userService;
     }
+
     public void OnGet()
     {
-        
     }
 
     public IActionResult OnPost()
     {
-       return RedirectToPage("Login");
+        return RedirectToPage("Login");
     }
 }

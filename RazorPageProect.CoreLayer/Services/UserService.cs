69198b93e9b9dfb684c5cecfc532717ce4b8587f
Mehdi Heidari <hymehdi84@gmail.com>
2025-02-01 15:36:59 +0300

    User service added

---
+++ RazorPageProect.CoreLayer/Services/UserService.cs
@@ -0,0 +1,20 @@
+﻿using RazorPageProect.CoreLayer.DTOs;
+using RazorPageProect.DataLayer.Context;
+
+namespace RazorPageProect.CoreLayer.Services;
+
+public class UserService :IUserService
+{
+    private readonly BlogContext _context;
+
+    public UserService(BlogContext context)
+    {
+        _context = context;
+    }
+    public void RegisterUser(UserRegisterDTO registerDto)
+    {
+        var FullNameExist = _context.Users.Any(u => u.FullName == registerDto.FullName);
+        if (FullNameExist)
+            throw new Exception();
+    }
+}
--- RazorPageProect.DataLayer/obj/Debug/net9.0/RazorPageProect.DataLayer.AssemblyInfo.cs
+++ RazorPageProect.DataLayer/obj/Debug/net9.0/RazorPageProect.DataLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPageProect.DataLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+babb754372193780ae6dd8e209bf04e2a93be691")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+776cb9df9c10746c667eea14eff91b9f4a4fba14")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPageProect.DataLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPageProect.DataLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
